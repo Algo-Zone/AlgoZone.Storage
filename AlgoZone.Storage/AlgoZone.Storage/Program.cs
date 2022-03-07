a523e1e85970b4a88a4ad3a79ed6a0cd29725857
Bram Hoven <bram.hoven@hotmail.com>
2022-03-07 08:49:04 +0100

    Add initiali solution and project

---
+++ AlgoZone.Storage/AlgoZone.Storage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AlgoZone.Storage
+{
+    public static class Program
+    {
+        #region Methods
+
+        #region Static Methods
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
